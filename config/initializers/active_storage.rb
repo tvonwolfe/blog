55b2f447bb1159e03c0ab12a6b324522244db833
Tony Von Wolfe <tonyvonwolfe@protonmail.com>
2025-12-31 21:20:16 -0700

    add separate credentials files per env

--- config/initializers/active_storage.rb
+++ config/initializers/active_storage.rb
@@ -15,7 +15,7 @@ Rails.application.config.to_prepare do
   ActiveStorage::DiskController.class_eval do
     include AdminSession
 
-    before_action :authorize_admin_session
+    before_action :authorize_admin_session, only: :update
 
     def authorize_admin_session
       return if admin?
