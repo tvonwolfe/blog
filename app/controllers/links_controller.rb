84c8186496daa95692203bb7f844028c4dd2ba54
Tony Von Wolfe <tonyvonwolfe@protonmail.com>
2026-04-20 08:41:46 -0600

    fix link ordering logic
    need to first perform a query that groups by target_domain only in order
    to get an accurate COUNT, then perform another query to retrieve the
    other data that's relevant for rendering. so overall, there will be 4
    queries per request. not _awful_.

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,10 +2,11 @@
 
 class LinksController < ApplicationController
   def index
-    @links = Link
-      .includes(:posts)
-      .select(:id, :target_domain, :target_url)
-      .group(:target_domain, :target_url, :id)
-      .order("COUNT(target_domain) DESC, target_url ASC")
+    @link_domains = Link
+      .select(:target_domain, "COUNT(target_domain IS NULL) as domain_freq")
+      .group(:target_domain)
+      .order("domain_freq DESC, target_domain ASC")
+
+    @links = Link.includes(:posts).select(:id, :target_domain, :target_url)
   end
 end
